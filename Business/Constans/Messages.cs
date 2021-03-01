993c70c1de1a2dd1aa2a163bf0674a5d465877d3
Kübra AKKAYA <200601010@bakircay.edu.tr>
2021-03-01 22:43:43 +0300

    01.03.2021

--- Business/Constans/Messages.cs
+++ Business/Constans/Messages.cs
@@ -1,6 +1,8 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constans
@@ -14,5 +16,12 @@ namespace Business.Constans
         public static string ProductCountOfCategoryError = "Bir kategoride en fazla 10 ürün olabilir";
         public static string ProductNameAlreadyExists = "Bu isimde zaten başka bir ürün var.";
         public static string CategoryLimitExceded = "Kategori limiti aşıldığı için yeni ürün eklenemez.";
+        public static string AuthorizationDenied = "Yetkiniz yok.";
+        public static string UserNotFound="Kullanıcı bulunamadı.";
+        public static string UserRegistered = "Kayıt oldu.";
+        public static string PasswordError = "Parola hatası.";
+        public static string SuccessfulLogin = "Başarılı giriş.";
+        public static string UserAlreadyExists = "Kullanıcı mevcuttur.";
+        public static string AccessTokenCreated = "Parola oluşturuldu.";
     }
 }
