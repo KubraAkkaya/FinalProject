1c292fb465e88e4b00e8934b49ef1b5081e96499
Kübra AKKAYA <200601010@bakircay.edu.tr>
2021-03-13 23:59:28 +0300

    13.03.2021

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -53,7 +53,7 @@ namespace Business.Concrete
         [CacheAspect]
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour == 22)
+            if (DateTime.Now.Hour == 05)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
